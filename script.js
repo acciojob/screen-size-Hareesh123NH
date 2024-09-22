189c223cd266b96dcccfafe72eff959968d12e5a
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-09-22 18:34:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+const size=document.getElementById("sizeInfo");
+size.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+window.addEventListener("resize",()=>{
+	size.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+});
